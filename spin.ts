16f63dff37009a06fb8203aff4d189f2d7f9eab2
duckpunk <ducreatif@gmail.com>
2025-06-23 04:33:20 +0700

    Delete spin.ts

--- spin.ts
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const hadiah = Math.floor(Math.random() * 100) + 1;
-  res.json({
-    message: `🎁 Kamu dapat ${hadiah} bola!`,
-    addBalls: hadiah
-  });
-}
