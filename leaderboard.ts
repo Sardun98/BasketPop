5985b11b61f4a4bf23aea7c7a8e111427f6ae510
duckpunk <ducreatif@gmail.com>
2025-06-23 04:30:39 +0700

    Delete leaderboard.ts

--- leaderboard.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  res.json({
-    topPlayers: [
-      { address: "0xUser1...", score: 17 },
-      { address: "0xUser2...", score: 12 },
-      { address: "0xUser3...", score: 9 },
-    ]
-  });
-}
